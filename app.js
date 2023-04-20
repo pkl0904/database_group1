6724eec6b729f344e9a7e20aa601f950469231cb
Umigod <127021526+Umigod@users.noreply.github.com>
2023-04-20 21:34:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -52,4 +52,21 @@ document.getElementById("close-btn").addEventListener("click", function() {
   document.getElementById("close-btn").addEventListener("click", function() {
     document.getElementById("model-content").style.display = "none";
   });
-      
+      function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search-bar");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table-data");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
